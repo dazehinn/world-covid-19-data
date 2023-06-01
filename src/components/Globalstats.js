e4b3a2eacd1106d3c3dc4273eb073d6f107dadf3
dazehinn <dazehinn@yahoo.fr>
2023-06-01 02:16:22 +0100

    Pass down props

--- src/components/Globalstats.js
+++ src/components/Globalstats.js
@@ -1,32 +1,37 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCovidData } from '../redux/statisticsSlice';
 import '../App.css'
 import { showDetails } from '../redux/statisticsSlice';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import CountryStats from './CountryStats';
 
 const Globalstats = () => {
   const { dataArray/*, isLoading */ } = useSelector((state) => state.Covid);
     const dispatch = useDispatch();
+
   
-  const handleClick = (country) => {
-    dispatch(showDetails(country))
-  }  
-    if (dataArray.length === 0) {
-        setTimeout(() => {
-          dispatch(fetchCovidData());
-        }, '1000');
-      }
+    // if (dataArray.length === 0) {
+    //     setTimeout(() => {
+    //       dispatch(fetchCovidData());
+    //     }, '1000');
+    //   }
+    
+    useEffect(() => {
+      dispatch(fetchCovidData());
+      }, [dispatch, fetchCovidData]);
+    
     
   return (
     <div className='countryContainer'>
         {dataArray.map((data) => (
-            <div key={data.country+data.region} className="countryCard" onClick={() => handleClick(data.country+data.region)}>
-                <h2>{data.country}</h2>
-                <h3>{data.region}</h3>
-            </div>
+          <CountryStats key={data.id} details={data} />
+          
         ))
 }
+        {/* <FaArrowRight className="fontIcon" /> */}
     </div>
   )}
 export default Globalstats;
