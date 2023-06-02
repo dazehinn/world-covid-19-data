b9d4e3b254dafaee039a07a1c9e1ce8d809183e0
dazehinn <dazehinn@yahoo.fr>
2023-06-02 11:57:28 +0100

    Add test for Details component

---
+++ src/__tests__/Details.test.js
@@ -0,0 +1,15 @@
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import Details from '../components/Details';
+import store from '../Redux/store';
+
+it('Test Details component renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Details />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
