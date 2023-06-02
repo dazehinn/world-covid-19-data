0cd4412e9388cb1e6e4476e219551a3f12c65c94
dazehinn <dazehinn@yahoo.fr>
2023-06-02 11:59:20 +0100

    Add test for GlobalStats component

---
+++ src/__tests__/GlobalStats.test.js
@@ -0,0 +1,15 @@
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../Redux/store';
+import GlobalStats from '../components/Globalstats';
+
+it('Test Globalstats renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <GlobalStats />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
